12c87189bdf49461fda1d921ec8d6eab4f73bbd7
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2025-06-16 15:59:11 +0800

    20250616

---
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/status/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ph.com.guanzongroup.cas.cashflow.status;
+
+/**
+ *
+ * @author Michael Cuison
+ */
+public class NewClass {
+    
+}
