1f9f73f84bbd8689cc6b44410e94f574039c93d6
Jeitee <93184955+jeitee@users.noreply.github.com>
2025-06-20 11:56:55 +0800

    062025
    add method search transaction for check printing request

--- src/main/java/ph/com/guanzongroup/cas/cashflow/CheckPrintingRequest.java
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/CheckPrintingRequest.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import org.guanzon.appdriver.agent.ShowDialogFX;
 import org.guanzon.appdriver.agent.services.Model;
 import org.guanzon.appdriver.agent.services.Transaction;
 import org.guanzon.appdriver.base.GuanzonException;
@@ -617,5 +618,44 @@ public class CheckPrintingRequest extends Transaction {
     public Model_Check_Printing_Master poCheckPrinting(int row) {
         return (Model_Check_Printing_Master) poCheckPrinting.get(row);
     }
+    
+    public JSONObject SearchTransaction(String fsValue) throws CloneNotSupportedException, SQLException, GuanzonException {
+        poJSON = new JSONObject();
+        String lsTransStat = "";
+        if (psTranStat.length() > 1) {
+            for (int lnCtr = 0; lnCtr <= psTranStat.length() - 1; lnCtr++) {
+                lsTransStat += ", " + SQLUtil.toSQL(Character.toString(psTranStat.charAt(lnCtr)));
+            }
+            lsTransStat = " AND a.cTranStat IN (" + lsTransStat.substring(2) + ")";
+        } else {
+            lsTransStat = " AND a.cTranStat = " + SQLUtil.toSQL(psTranStat);
+        }
+        initSQL();
+        String lsFilterCondition = String.join(
+                " b.sBranchCd = " + SQLUtil.toSQL(Master().getBranchCode()));
+        String lsSQL = MiscUtil.addCondition(SQL_BROWSE, lsFilterCondition);
+        if (!psTranStat.isEmpty()) {
+            lsSQL = lsSQL + lsTransStat;
+        }
+        lsSQL = lsSQL + " GROUP BY a.sTransNox";
+        System.out.println("SQL EXECUTED: " + lsSQL);
+        poJSON = ShowDialogFX.Browse(poGRider,
+                lsSQL,
+                fsValue,
+                "Transaction No»Transaction Date»Bank",
+                "a.sTransNox»a.dTransact»b.sBankName",
+                "a.sTransNox»a.dTransact»b.sBankName",
+                1);
+
+        if (poJSON != null) {
+            return OpenTransaction((String) poJSON.get("sTransNox"));
+        } else {
+            poJSON = new JSONObject();
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded.");
+            return poJSON;
+        }
+    }
+
 
 }
