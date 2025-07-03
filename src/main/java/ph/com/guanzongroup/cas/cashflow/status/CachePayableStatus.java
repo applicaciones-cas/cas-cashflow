7fbff1cc38f5dee91df9e7c3ec9eeec42d29c8c9
r-sie <32829606+Arsiela@users.noreply.github.com>
2025-07-03 11:58:17 +0800

    07032025 - Update SOA Tagging, AP Adjustment, Cache Payable Status
    - Update setting of cache payable status

---
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/status/CachePayableStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.com.guanzongroup.cas.cashflow.status;
+
+/**
+ *
+ * @author Arsiela
+ */
+public class CachePayableStatus {
+    public static final String OPEN = "0";
+    public static final  String CONFIRMED = "1";
+    public static final  String POSTED = "2"; 
+    public static final  String CANCELLED = "3";
+}
