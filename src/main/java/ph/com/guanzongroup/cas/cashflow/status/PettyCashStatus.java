e0fff14c9d85a1055eddaea59cf95fb7a44854b1
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-04-01 16:14:08 +0800

    04012026 - Update Model_PettyCash functionality
     - Update Model_PettyCash functionality

---
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/status/PettyCashStatus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.com.guanzongroup.cas.cashflow.status;
+
+/**
+ *
+ * @author Arsiela 04/01/2026
+ */
+public class PettyCashStatus { 
+    public static final String OPEN = "0";
+    public static final  String ACTIVE = "1";
+    public static final  String DEACTIVATED = "3";
+}
