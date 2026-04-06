fb5ef537a121e8fccee3f0c03d9d8eda0038ac4f
r-sie <32829606+Arsiela@users.noreply.github.com>
2026-04-06 10:00:15 +0800

    04062026 - Added functionality for Petty Cash Disbursement
     - Added functionality for Petty Cash Disbursement

---
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/model/Model_PettyCash_Disbursement.java
@@ -0,0 +1,412 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.com.guanzongroup.cas.cashflow.model;
+
+import java.util.Date;
+import java.sql.SQLException;
+import org.guanzon.appdriver.agent.services.Model;
+import org.guanzon.appdriver.base.GuanzonException;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.constant.EditMode;
+import org.guanzon.appdriver.constant.Logical;
+import org.guanzon.cas.client.model.Model_Client_Master;
+import org.guanzon.cas.client.services.ClientModels;
+import org.guanzon.cas.parameter.model.Model_Branch;
+import org.guanzon.cas.parameter.model.Model_Company;
+import org.guanzon.cas.parameter.model.Model_Department;
+import org.guanzon.cas.parameter.model.Model_Industry;
+import org.guanzon.cas.parameter.services.ParamModels;
+import org.json.simple.JSONObject;
+import ph.com.guanzongroup.cas.cashflow.services.CashflowModels;
+import ph.com.guanzongroup.cas.cashflow.status.CashAdvanceStatus;
+
+/**
+ *
+ * @author Arsiela 04-01-2026
+ */
+public class Model_PettyCash_Disbursement extends Model {
+
+    Model_Branch poBranch;
+    Model_Industry poIndustry;
+    Model_Company poCompany;
+    Model_Department poDepartment;
+    Model_Client_Master poCreditTo;
+    Model_Client_Master poClient;
+    Model_PettyCash poPettyCash;
+
+    @Override
+    public void initialize() {
+        try {
+            poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
+
+            poEntity.last();
+            poEntity.moveToInsertRow();
+
+            MiscUtil.initRowSet(poEntity);
+            //assign default values
+            poEntity.updateNull("dTransact");
+            poEntity.updateNull("dModified");
+            poEntity.updateObject("nEntryNox", 0);
+            poEntity.updateObject("nTranTotl", 0.0000);
+            poEntity.updateObject("nVATSales", 0.0000);
+            poEntity.updateObject("nVATAmtxx", 0.0000);
+            poEntity.updateObject("nZroVATSl", 0.0000);
+            poEntity.updateObject("nVatExmpt", 0.0000);
+            poEntity.updateObject("nWTaxTotl", 0.0000);
+            poEntity.updateObject("nNetTotal", 0.0000);
+            poEntity.updateString("cTranStat", CashAdvanceStatus.OPEN);
+            poEntity.updateString("cCollectd", Logical.NO);
+            poEntity.updateString("cVchrPrnt", Logical.NO);
+            //end - assign default values
+
+            poEntity.insertRow();
+            poEntity.moveToCurrentRow();
+            poEntity.absolute(1);
+
+            ID = "sTransNox";
+
+            //initialize reference objects
+            ParamModels model = new ParamModels(poGRider);
+            poBranch = model.Branch();
+            poIndustry = model.Industry();
+            poCompany = model.Company();
+            poDepartment = model.Department();
+
+            ClientModels clientModel = new ClientModels(poGRider);
+            poCreditTo = clientModel.ClientMaster();
+            poClient = clientModel.ClientMaster();
+
+            CashflowModels gl = new CashflowModels(poGRider);
+            poPettyCash = gl.PettyCashMaster();
+//            end - initialize reference objects
+
+            pnEditMode = EditMode.UNKNOWN;
+        } catch (SQLException e) {
+            logwrapr.severe(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public String getNextCode() {
+        return MiscUtil.getNextCode(this.getTable(), ID, true, poGRider.getGConnection().getConnection(), poGRider.getBranchCode());
+    }
+
+    public JSONObject setTransactionNo(String transactionNo) {
+        return setValue("sTransNox", transactionNo);
+    }
+
+    public String getTransactionNo() {
+        return (String) getValue("sTransNox");
+    }
+
+    public JSONObject setEntryNo(String entryNo) {
+        return setValue("nEntryNox", entryNo);
+    }
+
+    public String getEntryNo() {
+        return (String) getValue("nEntryNox");
+    }
+
+    public JSONObject setTransactionDate(Date transactionDate) {
+        return setValue("dTransact", transactionDate);
+    }
+
+    public Date getTransactionDate() {
+        return (Date) getValue("dTransact");
+    }
+
+    public JSONObject setVoucherNo(String voucherNo) {
+        return setValue("sVoucherx", voucherNo);
+    }
+
+    public String getVoucherNo() {
+        return (String) getValue("sVoucherx");
+    }
+
+    public JSONObject setCompanyId(String companyId) {
+        return setValue("sCompnyID", companyId);
+    }
+
+    public String getCompanyId() {
+        return (String) getValue("sCompnyID");
+    }
+
+    public JSONObject setBranchCode(String branchCode) {
+        return setValue("sBranchCd", branchCode);
+    }
+
+    public String getBranchCode() {
+        return (String) getValue("sBranchCd");
+    }
+
+    public JSONObject setIndustryId(String industryCode) {
+        return setValue("sIndstCdx", industryCode);
+    }
+
+    public String getIndustryId() {
+        return (String) getValue("sIndstCdx");
+    }
+
+    public JSONObject setPettyId(String pettyId) {
+        return setValue("sPettyIDx", pettyId);
+    }
+
+    public String getPettyId() {
+        return (String) getValue("sPettyIDx");
+    }
+    
+    public JSONObject setClientId(String clientId) {
+        return setValue("sClientID", clientId);
+    }
+
+    public String getClientId() {
+        return (String) getValue("sClientID");
+    }
+
+    public JSONObject setPayeeName(String payeeName) {
+        return setValue("sPayeeNme", payeeName);
+    }
+
+    public String getPayeeName() {
+        return (String) getValue("sPayeeNme");
+    }
+
+    public JSONObject setCreditedTo(String creditedTo) {
+        return setValue("sCrdtedTo", creditedTo);
+    }
+
+    public String getCreditedTo() {
+        return (String) getValue("sCrdtedTo");
+    }
+
+    public JSONObject setDepartmentRequest(String deptRequest) {
+        return setValue("sDeptReqs", deptRequest);
+    }
+
+    public String getDepartmentRequest() {
+        return (String) getValue("sDeptReqs");
+    }
+
+    public JSONObject setAddress(String address) {
+        return setValue("sAddressx", address);
+    }
+
+    public String getAddress() {
+        return (String) getValue("sAddressx");
+    }
+
+    public JSONObject setRemarks(String remarks) {
+        return setValue("sRemarksx", remarks);
+    }
+
+    public String getRemarks() {
+        return (String) getValue("sRemarksx");
+    }
+
+    public JSONObject setReferNo(String referNo) {
+        return setValue("sReferNox", referNo);
+    }
+
+    public String getReferNo() {
+        return (String) getValue("sReferNox");
+    }
+
+//    public JSONObject setSourceCode(String sourceCode) {
+//        return setValue("sSourceCd", sourceCode);
+//    }
+//
+//    public String getSourceCode() {
+//        return (String) getValue("sSourceCd");
+//    }
+
+//    public JSONObject setSourceNo(String sourceNo) {
+//        return setValue("sSourceNo", sourceNo);
+//    }
+//
+//    public String getSourceNo() {
+//        return (String) getValue("sSourceNo");
+//    }
+
+    public JSONObject setTransactionTotal(Double transactionTotal) {
+        return setValue("nTranTotl", transactionTotal);
+    }
+
+    public Double getTransactionTotal() {
+        if (getValue("nTranTotl") == null || "".equals(getValue("nTranTotl"))) {
+            return 0.0000;
+        }
+        return Double.valueOf(getValue("nTranTotl").toString());
+    }
+    
+    public boolean isPrinted() {
+        return ((String) getValue("cVchrPrnt")).equals("1");
+    }
+
+    public JSONObject isPrinted(boolean isPrinted) {
+        return setValue("cVchrPrnt", isPrinted ? "1" : "0");
+    }
+    
+    public JSONObject isCollected(boolean isCollected) {
+        return setValue("cCollectd", isCollected ? "1" : "0");
+    }
+
+    public boolean isCollected() {
+        return ((String) getValue("cProcessd")).equals("1");
+    }
+
+    public JSONObject setTransactionStatus(String transactionStatus) {
+        return setValue("cTranStat", transactionStatus);
+    }
+
+    public String getTransactionStatus() {
+        return (String) getValue("cTranStat");
+    }
+
+    public JSONObject setModifiedBy(String modifiedBy) {
+        return setValue("sModified", modifiedBy);
+    }
+
+    public String getModifiedBy() {
+        return (String) getValue("sModified");
+    }
+
+    public JSONObject setModifiedDate(Date modifiedDate) {
+        return setValue("dModified", modifiedDate);
+    }
+
+    public Date getModifiedDate() {
+        return (Date) getValue("dModified");
+    }
+
+    //reference object models
+    public Model_Branch Branch() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sBranchCd"))) {
+            if (poBranch.getEditMode() == EditMode.READY
+                    && poBranch.getBranchCode().equals((String) getValue("sBranchCd"))) {
+                return poBranch;
+            } else {
+                poJSON = poBranch.openRecord((String) getValue("sBranchCd"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poBranch;
+                } else {
+                    poBranch.initialize();
+                    return poBranch;
+                }
+            }
+        } else {
+            poBranch.initialize();
+            return poBranch;
+        }
+    }
+
+    public Model_Industry Industry() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sIndstCdx"))) {
+            if (poIndustry.getEditMode() == EditMode.READY
+                    && poIndustry.getIndustryId().equals((String) getValue("sIndstCdx"))) {
+                return poIndustry;
+            } else {
+                poJSON = poIndustry.openRecord((String) getValue("sIndstCdx"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poIndustry;
+                } else {
+                    poIndustry.initialize();
+                    return poIndustry;
+                }
+            }
+        } else {
+            poIndustry.initialize();
+            return poIndustry;
+        }
+    }
+
+    public Model_Company Company() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sCompnyID"))) {
+            if (poCompany.getEditMode() == EditMode.READY
+                    && poCompany.getCompanyId().equals((String) getValue("sCompnyID"))) {
+                return poCompany;
+            } else {
+                poJSON = poCompany.openRecord((String) getValue("sCompnyID"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poCompany;
+                } else {
+                    poCompany.initialize();
+                    return poCompany;
+                }
+            }
+        } else {
+            poCompany.initialize();
+            return poCompany;
+        }
+    }
+
+    public Model_Client_Master Credited() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sCrdtedTo"))) {
+            if (poClient.getEditMode() == EditMode.READY
+                    && poClient.getClientId().equals((String) getValue("sCrdtedTo"))) {
+                return poClient;
+            } else {
+                poJSON = poClient.openRecord((String) getValue("sCrdtedTo"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poClient;
+                } else {
+                    poClient.initialize();
+                    return poClient;
+                }
+            }
+        } else {
+            poClient.initialize();
+            return poClient;
+        }
+    }
+
+    public Model_Department Department() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sDeptReqs"))) {
+            if (poDepartment.getEditMode() == EditMode.READY
+                    && poDepartment.getDepartmentId().equals((String) getValue("sDeptReqs"))) {
+                return poDepartment;
+            } else {
+                poJSON = poDepartment.openRecord((String) getValue("sDeptReqs"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poDepartment;
+                } else {
+                    poDepartment.initialize();
+                    return poDepartment;
+                }
+            }
+        } else {
+            poDepartment.initialize();
+            return poDepartment;
+        }
+    }
+    
+    public Model_PettyCash CashFund() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sPettyIDx"))) {
+            if (poPettyCash.getEditMode() == EditMode.READY
+                    && poPettyCash.getPettyId().equals((String) getValue("sPettyIDx"))) {
+                return poPettyCash;
+            } else {
+                poJSON = poPettyCash.openRecord((String) getValue("sPettyIDx"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poPettyCash;
+                } else {
+                    poPettyCash.initialize();
+                    return poPettyCash;
+                }
+            }
+        } else {
+            poPettyCash.initialize();
+            return poPettyCash;
+        }
+    }
+
+}
---
+++ src/main/java/ph/com/guanzongroup/cas/cashflow/status/PettyCashDisbursementStatus.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.com.guanzongroup.cas.cashflow.status;
+
+/**
+ *
+ * @author Arsiela 04-01-2026
+ */
+public class PettyCashDisbursementStatus {
+    public static final String OPEN = "0";
+    public static final  String CONFIRMED = "1";
+    public static final  String APPROVED = "2";
+    public static final  String CANCELLED = "3";
+    public static final  String VOID = "4";
+    
+    public static final String DEFAULT_VOUCHER_NO = "00000001";
+    
+    public static class Reverse  {
+        public static final  String INCLUDE = "+"; 
+        public static final  String EXCLUDE = "-"; 
+    }
+    
+    public static class SourceCode  {
+        public static final  String CASHADVANCE = "CADV"; 
+    }
+    
+}
